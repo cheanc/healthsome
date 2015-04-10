a68ff7dd3b4620b99c641d76ffd640980a56359d
halendk <duclh2000@gmail.com>
2015-04-10 14:01:24 -0500

    login and signup

--- gen/com/mdt/healthsome/R.java
+++ gen/com/mdt/healthsome/R.java
@@ -10,6 +10,11 @@ package com.mdt.healthsome;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int background=0x7f060001;
+        public static final int invalid=0x7f060000;
+        public static final int text_color=0x7f060002;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -25,18 +30,43 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090009;
+        public static final int cancel=0x7f090008;
+        public static final int email=0x7f090007;
+        public static final int no_account=0x7f090005;
+        public static final int password=0x7f090004;
+        public static final int sign_in=0x7f090003;
+        public static final int sign_up=0x7f090006;
+        public static final int subtitle=0x7f090001;
+        public static final int title=0x7f090000;
+        public static final int username=0x7f090002;
     }
     public static final class layout {
-        public static final int activity_splash=0x7f030000;
+        public static final int activity_log_in=0x7f030000;
+        public static final int activity_mystudies_fragment=0x7f030001;
+        public static final int activity_sign_up=0x7f030002;
+        public static final int activity_splash=0x7f030003;
     }
     public static final class menu {
-        public static final int splash=0x7f070000;
+        public static final int log_in=0x7f080000;
+        public static final int mystudies=0x7f080001;
+        public static final int sign_up=0x7f080002;
+        public static final int splash=0x7f080003;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050003;
         public static final int app_name=0x7f050000;
+        public static final int cancel=0x7f05000a;
         public static final int hello_world=0x7f050001;
+        public static final int no_account=0x7f050007;
+        public static final int sign_in=0x7f050006;
+        public static final int sign_up=0x7f050008;
+        public static final int signup=0x7f05000b;
+        public static final int subtitle=0x7f050005;
+        public static final int title=0x7f050002;
+        public static final int title_activity_log_in=0x7f050004;
+        public static final int title_activity_mystudies_fragment=0x7f05000c;
+        public static final int title_activity_sign_up=0x7f050009;
     }
     public static final class style {
         /** 
@@ -60,10 +90,9 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
---
+++ src/com/mdt/healthsome/LogIn.java
@@ -0,0 +1,44 @@
+package com.mdt.healthsome;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+
+public class LogIn extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_log_in);
+	
+	
+		Button signinButton = (Button)findViewById(R.id.sign_in);
+		
+		 signinButton.setOnClickListener(new OnClickListener(){
+	        	public void onClick(View viewParam) {
+	        		TextView username = (TextView)findViewById(R.id.username);
+	        		TextView password = (TextView)findViewById(R.id.password);
+	        		if (username.getText().equals("vibs") && password.getText().equals("mdt")){
+	        			
+	        		}
+	        		username.setText("");
+	        		password.setText("");
+	        	}
+	     });
+		 
+		 TextView signup = (TextView)findViewById(R.id.sign_up);
+		 
+		 signup.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(LogIn.this, SignUp.class);
+		        startActivity(i);
+			}
+		 });
+	}
+}
